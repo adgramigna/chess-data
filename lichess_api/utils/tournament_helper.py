9af6d852d949a468fb4d508318909ac47ce9230c
Andrew Gramigna <adgramigna1@gmail.com>
2024-04-13 09:08:06 -0400

    initial view for overview

---
+++ lichess_api/utils/tournament_helper.py
@@ -0,0 +1,73 @@
+import requests
+import polars as pl
+import json
+import os
+from dotenv import load_dotenv
+from utils.classes import Tour, Round
+from msgspec.json import decode
+
+load_dotenv()
+
+
+def call_lichess_broadcasts_api(broadcast_tournament_id, round_info=None):
+    headers = {
+        "Content-Type": "application/json",
+        "X-Api-Key": os.environ.get("LICHESS_API_KEY"),
+    }
+
+    if round_info is None:
+        lichess_broadcast_endpoint = (
+            f"https://lichess.org/api/broadcast/{broadcast_tournament_id}"
+        )
+    else:
+        pass
+
+    response = requests.get(lichess_broadcast_endpoint, headers=headers)
+
+    # return response
+
+    result_json = response.json()
+
+    return result_json, lichess_broadcast_endpoint
+
+
+def parse_broadcast_tournament_base(tour_data, rounds_data, tournament_id):
+    tour_json = {}
+    tour_json["tournament_id"] = tour_data.id
+    tour_json["name"] = tour_data.name
+    tour_json["slug"] = tour_data.slug
+    tour_json["created_at"] = tour_data.createdAt
+    tour_json["tier"] = tour_data.tier
+    tour_json["image"] = tour_data.image
+    tour_json["url"] = tour_data.url
+    tour_json["leaderboard"] = tour_data.leaderboard
+
+    rounds_json = []
+    for i, chess_round in enumerate(rounds_data):
+        round_json_record = {}
+        round_json_record["round_id"] = chess_round.id
+        round_json_record["tournament_id"] = tournament_id
+        round_json_record["name"] = chess_round.name
+        round_json_record["slug"] = chess_round.slug
+        round_json_record["created_at"] = chess_round.createdAt
+        round_json_record["starts_at"] = chess_round.startsAt
+        round_json_record["url"] = chess_round.url
+        round_json_record["finished"] = chess_round.finished
+
+        rounds_json.append(round_json_record)
+
+    return tour_json, rounds_json
+
+
+def create_broadcast_base_dataframes(json_data, tournament_id):
+    lichess_tour = decode(json.dumps(json_data["tour"]), type=Tour)
+    lichess_rounds = decode(json.dumps(json_data["rounds"]), type=list[Round])
+
+    lichess_tour_final, lichess_rounds_final = parse_broadcast_tournament_base(
+        lichess_tour, lichess_rounds, tournament_id
+    )
+
+    lichess_tour_df = pl.DataFrame(lichess_tour_final)
+    lichess_rounds_df = pl.DataFrame(lichess_rounds_final)
+
+    return [lichess_tour_df, lichess_rounds_df]
