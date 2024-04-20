b5a3438543502f9107a58826c38c89484506d2a1
Andrew Gramigna <adgramigna1@gmail.com>
2024-04-20 10:31:45 -0400

    make many fields Optional present in untitled games

--- lichess_api/utils/classes/rounds.py
+++ lichess_api/utils/classes/rounds.py
@@ -4,17 +4,17 @@ from typing import Optional
 
 class Player(Struct):
     name: str
-    title: str
-    rating: int
-    fed: str
+    fed: Optional[str] = None
+    rating: Optional[int] = None
+    title: Optional[str] = None
     clock: Optional[int] = None  # centiseconds, time is seconds * 100
 
 
 class Game(Struct):
     id: str
     name: str
-    fen: str
     players: list[Player]
-    lastMove: str
     status: str  # "*" if game not settled
+    lastMove: Optional[str] = None
+    fen: Optional[str] = None  # Game hasn't started
     thinkTime: Optional[int] = None  # When game is still occurring
