ec0ff60c942f0dc3aa360b0bb281028d59a46e14
Andrew Gramigna <adgramigna1@gmail.com>
2024-04-22 21:52:18 -0400

    add accuracy and win perc

--- dbt/chessray/models/staging/lichess/stg_lichess__game_moves.sql
+++ dbt/chessray/models/staging/lichess/stg_lichess__game_moves.sql
@@ -1,3 +1,12 @@
+with add_win_perc as (
+    select *, 
+        case 
+            when not is_checkmate_countdown then 100 / (1 + exp(-0.00368208 * engine_evaluation_score * 100)) 
+            else 100 / (1 + exp(-0.00368208 * (3000 - engine_evaluation_score)))
+        end as white_win_percentage
+    from {{ source('lichess', 'game_moves') }}
+)
+
 select 
     surrogate_move_id,
     surrogate_game_id,
@@ -5,6 +14,25 @@ select
     move_number,
     starting_square,
     san,
+    case 
+        when left(san, 1) = 'K' then 'king'
+        when left(san, 1) = 'R' then 'rook'
+        when left(san, 1) = 'B' then 'bishop'
+        when left(san, 1) = 'N' then 'knight'
+        when left(san, 1) = 'Q' then 'queen'
+        else 'pawn'
+    end as piece_moved,
+    right(san, 1) = '+' or right(san, 1) = '#' as is_check,
+    right(san, 1) = '#' as is_checkmate,
+    position('x' in san) > 0 as is_capture,
+    white_win_percentage,
+    100 - white_win_percentage as black_win_percentage,
+    case 
+        when is_white_move then least(103.1668 * exp(-0.04354 * (lag(white_win_percentage) 
+            over(partition by surrogate_game_id order by row_number) - white_win_percentage)) - 3.1669, 100) 
+        when not is_white_move then least(103.1668 * exp(-0.04354 * (lag((100 - white_win_percentage)) 
+            over(partition by surrogate_game_id order by row_number) - (100 - white_win_percentage))) - 3.1669, 100) 
+    end as move_accuracy,
     clock_time,
     engine_evaluation_score,
     is_checkmate_countdown,
@@ -12,4 +40,4 @@ select
     nag,
     comment,
     fen
-from {{ source('lichess', 'game_moves') }}
+from add_win_perc
