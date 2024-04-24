053243c604fba9b48394765270eea30adcaa97df
Andrew Gramigna <adgramigna1@gmail.com>
2024-04-23 22:09:09 -0400

    hash surrogate key and small refactor

--- dbt/chessray/models/marts/player_game_info.sql
+++ dbt/chessray/models/marts/player_game_info.sql
@@ -7,25 +7,7 @@ game_outline as (
 ),
 
 game_detail as (
-    select 
-        surrogate_game_id,
-        is_white_move as is_white,
-        max(move_number) as num_moves,
-        count(*)/sum(1/move_accuracy) as player_accuracy,
-        sum(case when is_capture then 1 else 0 end) as num_captures,
-        sum(case when is_check then 1 else 0 end) as num_checks_given,
-        max(is_checkmate) as ends_in_checkmate,
-        sum(case when nag = 6 then 1 else 0 end) as num_inaccuracies,
-        sum(case when nag = 2 then 1 else 0 end) as num_mistakes,
-        sum(case when nag = 4 then 1 else 0 end) as num_blunders,
-        sum(case 
-                when nag = 2 or nag = 4 or nag = 6
-                then 1 else 0
-            end
-        ) as num_poor_moves,
-    from {{ ref('stg_lichess__game_moves') }}
-    where is_finished
-    group by 1,2
+    select * from {{ ref('int_moves_to_game_player_grain') }}
 ),
 
 game_headers as (
@@ -34,10 +16,12 @@ game_headers as (
 
 rounds_outline as (
     select * from {{ ref('stg_lichess__rounds_outline') }}
+    where is_finished
 ),
 
 initial_player_game_info as (
     select 
+        md5(game_players.player_name || '_' || game_players.game_id) as id,
         game_players.player_name,
         game_players.game_id,
         game_outline.round_id,
@@ -73,15 +57,7 @@ initial_player_game_info as (
 
 select 
 initial_player_game_info.* exclude(surrogate_game_id, is_white),
-game_detail.num_moves,
-game_detail.player_accuracy,
-game_detail.num_captures,
-game_detail.num_checks_given,
-game_detail.ends_in_checkmate,
-game_detail.num_inaccuracies,
-game_detail.num_mistakes,
-game_detail.num_blunders,
-game_detail.num_poor_moves,
+game_detail.* exclude(surrogate_game_id, is_white)
 from initial_player_game_info
 inner join game_detail on initial_player_game_info.surrogate_game_id = game_detail.surrogate_game_id
     and initial_player_game_info.is_white = game_detail.is_white
