804b6adc1e80ba6984cd8a1fa3fb9da9dc027f5c
Andrew Gramigna <adgramigna1@gmail.com>
2024-04-26 09:07:35 -0400

    add colloquial name

--- dbt/chessray/models/staging/lichess/stg_lichess__game_players.sql
+++ dbt/chessray/models/staging/lichess/stg_lichess__game_players.sql
@@ -1,11 +1,33 @@
+with game_players_augmented as (
+    select 
+        * exclude(name),
+        {{ clean_player_name("name") }} as player_name,
+    from {{ source('lichess', 'game_players') }}
+
+),
+
+game_players_further_augmented as (
+    select
+        *,
+        split_part(player_name, ', ', 1) as last_name,
+        split_part(player_name, ', ', 2) as first_name,
+    from game_players_augmented
+
+)
+
 select 
     surrogate_game_player_id,
-    {{ clean_player_name("name") }} as player_name,
+    player_name,
+    case 
+        when left(last_name, 4) = 'Nepo' then 'Nepo'
+        when left(first_name, 5) = 'Pragg' then 'Pragg'
+        when len(last_name) = 1 then first_name
+        else last_name
+    end as colloquial_name,
     game_id,
     color,
     is_white,
     rating,
     federation,
-    title,
-    clock
-from {{ source('lichess', 'game_players') }}  
+    title
+from game_players_further_augmented
