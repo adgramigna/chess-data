bbbcd92d2a571350e71205f4f90e2fe6869a76f0
Andrew Gramigna <adgramigna1@gmail.com>
2024-04-28 14:14:52 -0400

    rename

--- dbt/chessray/models/intermediate/maps/int_ids_map.sql
+++ dbt/chessray/models/intermediate/maps/int_ids_map.sql
@@ -12,14 +12,24 @@ game_players as (
 
 rounds_outline as (
     select * from {{ ref('stg_lichess__rounds_outline') }}
-)   
+)
 
 select
+    md5(game_players.player_name || '_' || game_players.game_id) as id,
+    game_players.is_white,
     game_players.game_id,
+    game_players.player_name,
     coalesce(
         game_headers_white.surrogate_game_id, 
         game_headers_black.surrogate_game_id
-    ) as surrogate_game_id
+    ) as surrogate_game_id,
+    rounds_outline.round_id,
+    rounds_outline.tournament_id,
+
+    /* Do we need? */
+    game_players.color,
+    game_players.is_white,
+    game_players.colloquial_name,
 from game_players
 inner join game_outline on game_players.game_id = game_outline.game_id
 inner join rounds_outline on game_outline.round_id = rounds_outline.round_id
@@ -27,4 +37,3 @@ left join game_headers as game_headers_white on rounds_outline.round_id = game_h
     and game_players.player_name = game_headers_white.white_player
 left join game_headers as game_headers_black on rounds_outline.round_id = game_headers_black.round_id
     and game_players.player_name = game_headers_black.black_player
-group by 1,2
