164402f83cf525330d05fb63ba30225ace2efdda
Andrew Gramigna <adgramigna1@gmail.com>
2024-04-28 14:16:46 -0400

    add winning advantage calcs

--- dbt/chessray/models/staging/lichess/stg_lichess__game_moves.sql
+++ dbt/chessray/models/staging/lichess/stg_lichess__game_moves.sql
@@ -1,12 +1,24 @@
-with moves_augmented as (
+with add_color_with_winning_advantage as (
+    select *,
+    --GOOD
+    case 
+        when engine_evaluation_score > 1.25 then 'white'
+        when engine_evaluation_score < -1.25 then 'black'
+        else 'neutral'
+    end as color_with_winning_advantage
+    from {{ source('lichess', 'game_moves') }}
+),
+
+moves_augmented as (
     select *, 
         case 
             when not is_checkmate_countdown then 100 / (1 + exp(-0.00368208 * engine_evaluation_score * 100)) 
             else 100 / (1 + exp(-0.00368208 * (3000 - engine_evaluation_score)))
         end as white_win_percentage,
+        lag(color_with_winning_advantage) over(partition by surrogate_game_id order by row_number) as previous_color_with_winning_advantage,
         lower(split_part(fen,' ',1)) as fen_lower,
-        replace(replace(replace(replace(lower(split_part(fen,' ',1)), 'b', ''),'n', ''), 'q', ''), 'r', '') as fen_no_majors,
-    from {{ source('lichess', 'game_moves') }}
+        replace(replace(replace(replace(lower(split_part(fen,' ',1)), 'b', ''),'n', ''), 'q', ''), 'r', '') as fen_no_majors
+    from add_color_with_winning_advantage
 )
 
 select 
@@ -42,6 +54,14 @@ select
         else 'no time pressure'
     end as time_pressure_category,
     engine_evaluation_score,
+    color_with_winning_advantage,
+    previous_color_with_winning_advantage != color_with_winning_advantage 
+        and color_with_winning_advantage = 'white' as is_white_winning_advantage,
+    previous_color_with_winning_advantage != color_with_winning_advantage 
+        and color_with_winning_advantage = 'black' as is_black_winning_advantage,
+    previous_color_with_winning_advantage != color_with_winning_advantage 
+    (previous_color_with_winning_advantage = 'white' and color_with_winning_advantage != 'white' and is_white_move) 
+        or (previous_color_with_winning_advantage = 'black' and color_with_winning_advantage != 'black' and not is_white_move) as is_lost_winning_advantage,
     is_checkmate_countdown,
     is_white_move,
     nag,
