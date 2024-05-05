796a77a993ba34409d1b5a1e3200a93837a0261c
Andrew Gramigna <adgramigna1@gmail.com>
2024-05-05 13:42:21 -0400

    add players and colloquial players

--- dbt/chessray/models/marts/evidence/game_info.sql
+++ dbt/chessray/models/marts/evidence/game_info.sql
@@ -33,6 +33,10 @@ ids_map as (
         tournament_id
     from {{ ref('int_ids_map') }}
     group by all
+),
+
+white_and_black_players as (
+    select * from {{ ref('int_players_to_white_and_black') }}
 )
  
 select
@@ -42,10 +46,15 @@ select
     game_outline.game_status,
     game_headers.chess_variant,
     game_headers.opening_general,
+    white_and_black_players.white_player,
+    white_and_black_players.black_player,
+    white_and_black_players.white_player_colloquial,
+    white_and_black_players.black_player_colloquial,
     first_game_move.first_move,
     player_game_info.* exclude(game_id)
 from ids_map
 inner join game_outline on ids_map.game_id = game_outline.game_id
 inner join game_headers on ids_map.surrogate_game_id = game_headers.surrogate_game_id
+inner join white_and_black_players on ids_map.game_id = white_and_black_players.game_id
 inner join player_game_info on ids_map.game_id = player_game_info.game_id
 inner join first_game_move on ids_map.surrogate_game_id = first_game_move.surrogate_game_id
