3956f726018ac3047309aefe8f92ff78064bb07c
Andrew Gramigna <adgramigna1@gmail.com>
2024-05-05 13:41:29 -0400

    add columns

--- dbt/chessray/models/intermediate/int_player_moves_with_ids.sql
+++ dbt/chessray/models/intermediate/int_player_moves_with_ids.sql
@@ -4,15 +4,22 @@ with game_detail as (
 
 ids_map as (
     select * from {{ ref('int_ids_map') }}
+),
+
+white_and_black_players as (
+    select * from {{ ref('int_players_to_white_and_black') }}
 )
 
 select
     game_detail.surrogate_move_id,
     game_detail.is_white_move,
     ids_map.player_name,
+    ids_map.colloquial_name,
     ids_map.game_id,
     ids_map.round_id,
     ids_map.tournament_id,
+    white_and_black_players.white_player_colloquial,
+    white_and_black_players.black_player_colloquial,
     game_detail.starting_square,
     game_detail.san,
     game_detail.is_endgame,
@@ -24,9 +31,11 @@ select
     game_detail.is_checkmate,
     game_detail.is_capture,
     game_detail.move_accuracy,
+    game_detail.white_win_percentage,
     game_detail.time_spent_on_move,
     game_detail.is_checkmate_countdown,
     game_detail.engine_evaluation_score,
+    game_detail.massaged_engine_evaluation_score,
     game_detail.color_with_winning_advantage,
     game_detail.is_given_away_winning_advantage,
     game_detail.is_lost_winning_advantage,
@@ -37,4 +46,5 @@ select
 from game_detail
 inner join ids_map on game_detail.surrogate_game_id = ids_map.surrogate_game_id
     and game_detail.is_white_move = ids_map.is_white
+inner join white_and_black_players on ids_map.game_id = white_and_black_players.game_id
 
