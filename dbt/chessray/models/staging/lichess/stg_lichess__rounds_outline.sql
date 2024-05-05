f04fc925a56ee69797d27a1c52f0223f831a82f4
Andrew Gramigna <adgramigna1@gmail.com>
2024-05-05 17:50:39 -0400

    fix timestamp cols and add round number

--- dbt/chessray/models/staging/lichess/stg_lichess__rounds_outline.sql
+++ dbt/chessray/models/staging/lichess/stg_lichess__rounds_outline.sql
@@ -3,8 +3,9 @@ select
     tournament_id,
     name as round_name,
     slug,
-    to_timestamp(created_at) as created_at,
-    to_timestamp(starts_at) as starts_at,
+    epoch_ms(created_at) as created_at,
+    epoch_ms(starts_at) as starts_at,
+    row_number() over(partition by tournament_id order by starts_at) as round_number,
     url as round_url,
     finished as is_finished
 from {{ source('lichess', 'rounds_outline') }}
